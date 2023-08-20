be8cfc98c072bf81091ddc66b6f0e5deefe3afc8
sanpitka <sanpitka@student.oulu.fi>
2023-08-20 21:54:42 +0300

    2.6

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    console.log(persons)
+  }
+
+  const handleChanges = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleChanges}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
-
 }
 
 export default App
