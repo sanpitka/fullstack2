f468d6148468868ca08e9bbea0142f2e76e3a888
sanpitka <sanpitka@student.oulu.fi>
2023-09-10 21:40:26 +0300

    2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return(
@@ -68,15 +69,20 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const handleNameChanges = (event) => {
     setNewName(event.target.value)
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+import axios from 'axios'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+    const persons = response.data
+    ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons}/>)
+})
