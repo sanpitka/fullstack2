bed661170a2713a6d6dfe3505bc487b83597ac73
sanpitka <sanpitka@student.oulu.fi>
2023-12-22 23:23:24 +0200

    starting the data of countries exercise

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/*import { useState, useEffect } from 'react'
+import countries from 'https://studies.cs.helsinki.fi/restcountries/api/all'*/
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>
+      find countries
+    </div>
+  )
+}
+
+export default App
