e8c004d18853e064654f9221ca1097922d1aa4a9
sanpitka <sanpitka@student.oulu.fi>
2023-12-27 10:27:12 +0200

    2.20

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
+const api_key = import.meta.env.VITE_WEATHER_KEY
+
 const Countries = (props) => {
   
   function checkString(country) {
     return country.name.common.toLowerCase().includes(props.filter.toLowerCase())
   }
-
+  
   const countryList = props.countries.filter(checkString)
 
   if (props.filter.length < 1) {
@@ -34,6 +36,8 @@ const Countries = (props) => {
               )}
           </ul>
           <img src={country.flags.png} width="150px" border="1px" />
+          <h2>Weather in {country.capital}</h2>
+          <Weather capitalInfo={country.capitalInfo}/> 
         </div>
         )}
       </div>
@@ -43,6 +47,7 @@ const Countries = (props) => {
     <div>{countryList.map(country => 
       <div key={country.name.official}>
         {country.name.common}
+        <button onClick={() => props.showOneCountry(country.name.common)}>Show</button>
       </div>
       )}
     </div>
@@ -58,6 +63,32 @@ const Filter = (props) => {
       />
     </div>
   )
+}
+
+const Weather = (props) => {
+
+  const [weather, setWeather] = useState([])
+  
+  const lat = props.capitalInfo.latlng[0]
+  const lon = props.capitalInfo.latlng[1]
+  
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+  
+  if (weather.length != 0) return(
+  <div key = {props.name}>
+    temperature {weather.main.temp} Celsius <br />
+    <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/> <br />
+    wind {weather.wind.speed} m/s
+  </div>
+  )
+  
+  
 }
 
 const App = () => {
@@ -78,10 +109,14 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const showOneCountry = (name) => {
+    setFilter(name)
+  }
+
   return (
     <div>
       <Filter filter={filter} handleFilterChanges={handleFilterChanges}/>
-      <Countries countries={countries} filter={filter}/>
+      <Countries countries={countries} filter={filter} showOneCountry={showOneCountry}/>
     </div>
   )
 }
