d169ebc2e98aa830b69880e4d2feac008b3fc851
sanpitka <sanpitka@student.oulu.fi>
2024-05-11 20:52:11 +0300

    3.21

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://phonebook-fq98.onrender.com',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     }
